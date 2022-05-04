779772dd5834530c418dd67f7a55214e3cb069cc
Witawe <texys@list.ru>
2022-05-04 16:13:42 +0900

    deleted functions

--- classes/getExam.py
+++
@@ -1,6 +0,0 @@
-from main import Exam
-import openpyxl
-from datetime import date
-
-def getExam(file, group, subject, data):
-    pass
--- classes/getExamResult.py
+++
@@ -1,6 +0,0 @@
-from main import ExamPoints, Exam
-import openpyxl
-from datetime import date
-
-def getExamResult(file, exam:Exam):
-    pass
